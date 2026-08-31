b6f5374b54b8742bec7533ae2b817ca4bc45b496
djcahyono <dj.cahyono@gmail.com>
2026-08-31 08:58:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+DATA:
+Kelas, suhu dinding, jumlah AC, merk AC, jumlah remot, merk remot
+
+TO-DO LIST:
+Grafik data, datanya sendiri
---
+++ src/main.py
@@ -0,0 +1,8 @@
+class ACKelas:
+    def __init__(self, suhuDinding, jumlahAC, merkAC, jumlahRemot, merkRemot):
+        self.suhuDinding = suhuDinding
+        self.jumlahAC = 2 
+        self.merkAC = merkAC
+        self.jumlahRemot = jumlahRemot
+        self.merkRemot = merkRemot
+        self.jumlahAC = jumlahAC
